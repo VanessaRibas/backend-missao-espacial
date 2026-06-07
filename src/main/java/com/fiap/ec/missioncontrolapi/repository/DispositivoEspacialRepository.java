8e3847cc610d6e125e6413f2571a45f9575fa9d4
VanessaRibas <vanessairis545@gmail.com>
2026-06-07 15:31:04 -0300

    feat: implementando repositorio DispositivoEspacialRepository

---
+++ src/main/java/com/fiap/ec/missioncontrolapi/repository/DispositivoEspacialRepository.java
@@ -0,0 +1,5 @@
+package com.fiap.ec.missioncontrolapi.repository;
+import com.fiap.ec.missioncontrolapi.model.DispositivoEspacial;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface DispositivoEspacialRepository extends JpaRepository<DispositivoEspacial, Long> {
+}
