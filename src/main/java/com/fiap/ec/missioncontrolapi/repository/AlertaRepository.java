27bfbadee80f1022c2b53cf7aeaa000f88042aea
VanessaRibas <vanessairis545@gmail.com>
2026-06-07 15:42:50 -0300

    feat: implementando repositorio AlertaRepository

---
+++ src/main/java/com/fiap/ec/missioncontrolapi/repository/AlertaRepository.java
@@ -0,0 +1,6 @@
+package com.fiap.ec.missioncontrolapi.repository;
+import com.fiap.ec.missioncontrolapi.model.Alerta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlertaRepository extends JpaRepository<Alerta, Long> {
+}
