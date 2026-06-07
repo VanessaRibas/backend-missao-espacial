0be834f451d23f318e9e60d30da3b42643b1c87d
VanessaRibas <vanessairis545@gmail.com>
2026-06-07 14:18:34 -0300

    feat: criação da camada servise de Sensor

---
+++ src/main/java/com/fiap/ec/missioncontrolapi/service/SensorService.java
@@ -0,0 +1,25 @@
+package com.fiap.ec.missioncontrolapi.service;
+
+import com.fiap.ec.missioncontrolapi.model.Sensor;
+import com.fiap.ec.missioncontrolapi.repository.SensorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SensorService {
+
+    private final SensorRepository repository;
+
+    public SensorService(SensorRepository repository) {
+        this.repository = repository;
+    }
+
+    public Sensor salvar(Sensor sensor) {
+        return repository.save(sensor);
+    }
+
+    public List<Sensor> listar() {
+        return repository.findAll();
+    }
+}
