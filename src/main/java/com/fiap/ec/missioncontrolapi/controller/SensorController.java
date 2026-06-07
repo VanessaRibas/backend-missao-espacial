f9567ff4af519f9fff7d2db4851658ee4721e2dd
VanessaRibas <vanessairis545@gmail.com>
2026-06-07 14:22:20 -0300

    feat: criação do controller do Sensor

---
+++ src/main/java/com/fiap/ec/missioncontrolapi/controller/SensorController.java
@@ -0,0 +1,29 @@
+package com.fiap.ec.missioncontrolapi.controller;
+
+import com.fiap.ec.missioncontrolapi.model.Sensor;
+import com.fiap.ec.missioncontrolapi.service.SensorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sensores")
+@CrossOrigin
+public class SensorController {
+
+    private final SensorService service;
+
+    public SensorController(SensorService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Sensor criar(@RequestBody Sensor sensor) {
+        return service.salvar(sensor);
+    }
+
+    @GetMapping
+    public List<Sensor> listar() {
+        return service.listar();
+    }
+}
