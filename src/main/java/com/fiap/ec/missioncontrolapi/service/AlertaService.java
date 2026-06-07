412dc3056cd78c61516c085b7bddcf80ec1aadf9
VanessaRibas <vanessairis545@gmail.com>
2026-06-07 15:44:31 -0300

    feat: criação da camada service do AlertaService

---
+++ src/main/java/com/fiap/ec/missioncontrolapi/service/AlertaService.java
@@ -0,0 +1,25 @@
+package com.fiap.ec.missioncontrolapi.service;
+
+import com.fiap.ec.missioncontrolapi.model.Alerta;
+import com.fiap.ec.missioncontrolapi.repository.AlertaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlertaService {
+
+    private final AlertaRepository repository;
+
+    public AlertaService(AlertaRepository repository) {
+        this.repository = repository;
+    }
+
+    public Alerta salvar(Alerta alerta) {
+        return repository.save(alerta);
+    }
+
+    public List<Alerta> listar() {
+        return repository.findAll();
+    }
+}
